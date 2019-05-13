3afee96c628b19c47d61abb741f6180b95eae697
Reggie <reggiepuksta@gmail.com>
2019-05-13 10:52:44 +0100

    Fixed file name conflict while loading in Windows. Might need to setup git configuration.

--- src/js/game/card/cardView.js
+++ src/js/game/card/cardView.js
@@ -1,42 +1,116 @@
 define(function(require) {
-    function getEl(name, ele) {
-        return document.getElementById(name);
+    //TODO REFACTORING this class into HandView without jQuery
+    var $ = require("jquery");
+    var cardsModel = require("cardsModel");
+    var utils = require("utils");
+    //Hover cards, Version 3
+    // Need more fine tuned controll over each card and
+    // String before appending it to dom
+    // Create an array of card coordinations
+    // appending it to DOM
+
+    function CardsView(container, cardsFrame) {
+        this.$container = container || $(".cards-player-area");
+        this.cardsFrame = cardsFrame || "card-frame";
+        this.data = cardsModel.getData();
+        this.$cardsFrame = {};
+        this.width = 30;
+        this.cardsCoord = []; // This is where the main string will be hold before
     }
 
-    function getElClass(ele, name) {
-        return ele.getElementsByClassName(name);
-    }
-    var Template = require('template');
-    var CardView = function() {
-        this.element = {};
-        this.effectStack = [];
-    };
-    CardView.prototype.place = function(pos) {
-        Animation.place(this, pos);
-    };
-    CardView.prototype.addEffect = function(effect, options) {
-        this.effectStack.push([effect, options]);
-        Animation.initEffect(effect, options);
-    };
-    CardView.prototype.updateStats = function(stats) {
-        if (stats) {
-            Template.update(stats);
+    CardsView.events = {};
+    // TODO We will handle DOM events later for now I leave this
+    // function here
+    CardsView.prototype.attach = function(el, events) {
+        console.log("Attaching");
+        for (var selector in events) {
+            if (events.hasOwnProperty(selector)) {
+                console.log("Yes");
+                var parts = selector.slice(" ", 2);
+                var callback = events[i];
+                $(el).on(parts[0], parts[1], callback);
+            }
         }
     };
-    CardView.prototype.getEleClass = function(ele, name) {
-        return ele.getElementsByClassName(name)[0];
+
+    CardsView.getCardsCoord = function() {
+        return this.cardsCoord;
     };
-    CardView.prototype.reduceHp = function(hp) {
-        var elementHp = this.getEleClass(this.element, 'hp');
-        elementHp.backgroundColor(red);
-        elementHp.innerHTML = hp;
-        // this.element.hp(hp);
-        // Animate.card.reduceHp(hp);
+
+    /* @param {type} name description */
+    CardsView.prototype.init = function() {
+        this.render(this.width);
+        // We can only find cards frame elements after they were attached
+        this.$cardsFrame = $("." + this.cardsFrame);
+        // cardsCoord = setCardCoord(4);
     };
-    CardView.prototype.increaseHp = function(hp) {
-        Animate.increaseHp(hp);
+
+    // Cards View coordinates are set on initialization
+    CardsView.prototype.setCardCoord = function() {
+        sectionsNum = this.data.numberOfCards;
+        // If all cards combined width is smaller than
+        // container, dont overlap cards and dont
+        // use card pushing animation
+        if (this.width * sectionsNum <= 100) {
+            var difference = 100 - (this.width * sectionsNum);
+            var marginRight = difference / sectionsNum - 1;
+            for (var i = 0; i < sectionsNum - 1; i++) {
+                // First one is always 0
+                if (i === 0) {
+                    this.cardsCoord.push(0);
+                }
+                this.cardsCoord.push(this.width * i + marginRight);
+            }
+            return this.cardsCoord;
+        }
+        // How much of overlaping is needed to fit the cards inside
+        // container
+        // We need to make space for push animation after the last card
+        var o = -(100 - (this.width * (sectionsNum + 1))) / (sectionsNum);
+        var overlapDistance = this.width - o;
+        // if width is specified we align sections according to the width
+        for (var j = 0; j < sectionsNum; j++) {
+            this.cardsCoord.push(j * overlapDistance);
+        }
     };
-    CardView.prototype.render = function(model) {
-        Template.render('card', model);
+
+    function positionCards() {
+        $ul.children().each(function(i, a) {
+            $(this).css("left", this.cardsCoord[i] + "%");
+        });
+    }
+
+    // @param {Array} cardsCoord Left positions of each card
+    CardsView.prototype.render = function() {
+        // Get 6 card coordinates
+        this.setCardCoord();
+        // TODO Modular Hover functionality
+        var cards = this.data.cardsList;
+        var cardHtml = [];
+        // Get card placement container div
+        // List all cards
+        for (var i = 0, n = this.cardsCoord.length; i < n; i++) {
+            // cardHtml = [];
+            var card = cards[i];
+            // build html
+            cardHtml.push("<li class='" + this.cardsFrame + "' ",
+
+                'style="left:' + this.cardsCoord[i] + '%;" ',
+                "data-pos='" + card.pos + "' ",
+                "data-card-id='" + card.cardId + "'>",
+
+                "<div class='card-name'>" + card.name + "</div>",
+                "<div class='card-cost'>" + card.cost + "</div>",
+                "<div class='card-stats'>" + card.stats + "</div>",
+                "<div class='card-stats'>" + card.currentLife + "</div>");
+            // How to inject into css the card image??
+            cardHtml.push("<div class='card-art' src='" + card.art + "'></div>");
+            cardHtml.push("</li>");
+        }
+        this.$container.append(cardHtml.join(""));
+        console.log("Appended");
+        this.attach(this.cardsFrame, CardsView.events);
+        // positionCards();
     };
+    return CardsView;
 });
--- src/js/game/card/cardsStorage.js
+++ src/js/game/card/cardsStorage.js
@@ -1,4 +1,42 @@
 define(function(require) {
+  'use strict';
+  var CardFactory = require('CardFactory');
+  var CardModel = require('CardModel');
+  var CardController = require('CardController');
+  var CardView = require('CardView');
+  cardFactory = new CardFactory();
+
+  var CardsStorage = function() {
+    this.cards = [];
+  };
+  CardsStorage.prototype.init = function(cardsData) {
+    for (var i = 0, len = cardsData.length; i < len; i++) {
+      var cardModel = new CardModel(cardsData[i]);
+      var cardView = new CardView(cardsData[i].type);
+      var cardController = new CardController(cardModel, cardView);
+      cardController.strategy(cardsData[i].strategy);
+      this.cards.push({
+        id: cardsData[i].id,
+        internalId: cardsData[i].internalId,
+        type: cardsData[i].type,
+        pos: cardsData[i].pos,
+        name: cardsData[i].name,
+        cost: cardsData[i].cost,
+        stats: cardsData[i].stats,
+        hp: cardsData[i].hp,
+        instance: cardController
+      });
+    }
+  };
+  CardsStorage.prototype.findByInternalId = function(id) {
+    this.cards.forEach(function(card, index) {
+      if (card.internalId === id) {
+        return card;
+      }
+    });
+  };
+
+  /*
     var CardsStorage = function() {
         this.cardsInstancesList = {};
     };
@@ -14,5 +52,6 @@ define(function(require) {
             this.cardsInstanceList[cardsDataArray.cardDeckId] = this.buildOne(cardsDataArray);
         }
     };
+    */
 
 });
