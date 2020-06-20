c67213fe929b970885151f1a248f136c7a92d9ba
ReggiePuksta <reggiepuksta@gmail.com>
2020-06-20 16:30:38 +0100

    feat: add game layout styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,39 +1,26 @@
-//TODO simplify Set Up Loading page
-var Data = require('data');
-var BoardModel = require('boardModel');
-var HandModel = require('handModel');
-// var HeroModel = require('heroModel');
-// var GameView = require('gameView');
-var BoardView = require('boardView');
-var HandView = require('handView');
-// var HeroView = require('heroView');
-var BoardController = require('boardController');
-var HandController = require('handController');
-// var HeroController = require('heroController');
+/* Game initialisation procedure */
+/* Before the game starts we have:
+    + Basic MVC description classes: Model, View, Ctrl
+    + TemplateStorage
+    + UserData
+    + GameData:
+  4) Instantiate all views. 
+  5) Instantiate all controllers - connect models and views.
+  6) Show all views in order:
+    + GameView - Background, UserView.
+    + Board
+    + Player and enemy heroes -> show animation appearing
+    + Player and enemy Hand -> show animation dealing
+*/
 
-var data = new Data();
-// Get all the game information before starting match
-data.init(function(matchData) {
-  // Populate game models
-  var board = new BoardModel(matchData.boardState);
-  var hand = new HandModel(matchData.playerCards);
-  // var hero = new HeroModel(data);
+import GameView from "./game/game-view.js";
 
-  // Create Views
-  // GameVew acts as a placeholder for different game elements
-  // like Board and players hand
-  var gameView = new GameView();
-  var boardView = new BoardView();
-  var handView = new HandView();
+import "../css/normalize.css";
+import "../css/style.css";
 
-  // Controllers initialise Views with Models data
-  var boardController = new BoardController(board, boardView);
-  var handController = new HandController(hand, handView);
-  // var heroController = new HeroController(hero, heroView);
-  // var userController = new UserController(user, userView);
-  // var heroView = new heroView();
-  gameView.addComponent(boardView);
-  gameView.addComponent(handView);
-  // gameView.addComponent(heroView);
-  gameView.init();
-});
+// WebSocket.on("load", function (data) {
+//   GameModel.update(data);
+// });
+
+// gameView.renderAll();
+// Example of Incomming Data from websockets
