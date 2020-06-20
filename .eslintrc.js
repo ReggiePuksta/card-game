ceb19d0610ed6c2bae5dd247cd5b9b74ff42d86e
ReggiePuksta <reggiepuksta@gmail.com>
2020-06-20 16:24:57 +0100

    build: add babel and css loaders to webpack

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
 module.exports = {
   env: { node: true, commonjs: true, jest: true, browser: true, es6: true },
   plugins: ["prettier", "promise"],
+  parserOptions: {
+    ecmaVersion: 8,
+    sourceType: "module",
+  },
   extends: [
     "eslint:recommended",
     "plugin:prettier/recommended",
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,41 @@
-var path = require('path');
-  module.exports = {
-    entry:  path.resolve(__dirname,"./src/js/game/main.js"),
-    devServer: {
-      contentBase: "./dist",
-      port: 9000
-      // hot: true
-    },
-    devtool: 'inline-source-map',
-    mode: "development",
-    module: {
-      rules: []
-    },
-    plugins: [],
-    output: {
-      filename: "bundle.js",
-      path: path.resolve(__dirname, "dist")
-    },
-  }
+var path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "./src/js/main.js"),
+  devServer: {
+    contentBase: "./dist",
+    port: 9000,
+    host: "0.0.0.0",
+    // hot: true,
+  },
+  devtool: "inline-source-map",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
